4af17e10ebb4cd3990813c10d18afabf0ba8418b
lego <lego@MusicAILabMacBook-Air.local>
2022-05-12 16:41:44 +0800

    update script

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 $(document).ready(function () {
 
     urls  = [
-        'https://zh.surveymonkey.com/r/3FWQGVZ](https://zh.surveymonkey.com/r/3FWQGVZ',
-        'https://zh.surveymonkey.com/r/G3RTGXH](https://zh.surveymonkey.com/r/G3RTGXH',
-        'https://zh.surveymonkey.com/r/G3F9SYD](https://zh.surveymonkey.com/r/G3F9SYD',
-        'https://zh.surveymonkey.com/r/3FPQ9YQ](https://zh.surveymonkey.com/r/3FPQ9YQ',
-        'https://zh.surveymonkey.com/r/G3PS8B9](https://zh.surveymonkey.com/r/G3PS8B9',
+        'https://zh.surveymonkey.com/r/3FWQGVZ',
+        'https://zh.surveymonkey.com/r/G3RTGXH',
+        'https://zh.surveymonkey.com/r/G3F9SYD',
+        'https://zh.surveymonkey.com/r/3FPQ9YQ',
+        'https://zh.surveymonkey.com/r/G3PS8B9',
     ]
     
     
